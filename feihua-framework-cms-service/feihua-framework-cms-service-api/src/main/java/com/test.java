d8800e18d8ea19a2f8b55d077031396e3ef63a11
revolver <595009116@qq.com>
2018-11-09 13:33:04 +0800

    添加cms模块

---
+++ feihua-framework-cms-service/feihua-framework-cms-service-api/src/main/java/com/test.java
@@ -0,0 +1,9 @@
+package com;
+
+/**
+ * @Auther: wzn
+ * @Date: 2018/11/9 13:31
+ * @Description:
+ */
+public class test {
+}
---
+++ feihua-framework-cms-service/feihua-framework-cms-service-impl/src/main/java/com/test.java
@@ -0,0 +1,9 @@
+package com;
+
+/**
+ * @Auther: wzn
+ * @Date: 2018/11/9 13:32
+ * @Description:
+ */
+public class test {
+}
